9a8900ef20c3b716e194ae7d91445e5d739adf9e
Subhan Nur Iqbal hakim <subhanhakim07@gmail.com>
2022-08-08 23:39:46 +0700

    make Data Resto

---
+++ src/data.js
@@ -0,0 +1,25 @@
+// import icons
+import Fee from "./assets/image/icons/fee.png";
+import ComeEat from "./assets/image/icons/come_eat.png";
+import Discount from "./assets/image/icons/discount.png";
+import StartGuard from "./assets/image/icons/startguard.png";
+import Verifikasi from "./assets/image/icons/verifikasi.png";
+
+// import Resto image
+import Batavia from "./assets/image/resto/batavia.png";
+import Dazza from "./assets/image/resto/dazza.png";
+import Resto1 from "./assets/image/resto/resto1.png";
+import Resto2 from "./assets/image/resto/resto2.png";
+import Resto3 from "./assets/image/resto/resto3.png";
+import Split from "./assets/image/resto/split.png";
+
+// import image
+import Chef from "./assets/image/chef.png";
+
+export const restoData = [
+  {
+    id: 1,
+    type: "Resto",
+    name: "Bind Balorant",
+  },
+];
